c0e6513aee89b39d31169aaa8baabdbe928eba28
Saad Nauman <87758531+saadings@users.noreply.github.com>
2024-07-09 16:30:17 +0500

    Add SMS service

--- src/lib/actions/appointment.actions.ts
+++ src/lib/actions/appointment.actions.ts
@@ -5,10 +5,12 @@ import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
+  messaging,
 } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import { formatDateTime, parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite";
 import { revalidatePath } from "next/cache";
+import { FormType } from "@/enums/form";
 
 export const createAppointment = async (
   appointment: CreateAppointmentParams,
@@ -114,11 +116,31 @@ export const updateAppointment = async ({
 
     if (!updatedAppointment) throw new Error("Appointment not found");
 
-    // SMS notification
+    const smsMessage = `Hi, its CarePulse.
+      ${type === FormType.SCHEDULE ? `Your appointment has been scheduled for ${formatDateTime(appointment.schedule!).dateTime} with Dr. ${appointment.primaryPhysician}` : `We regret to inform you that your appointment has been cancelled for the following reason: ${appointment.cancellationReason}`}.
+    `;
+
+    await sendSMSNotification(userId, smsMessage);
 
     revalidatePath("/admin");
 
     return parseStringify<Models.Document>(updatedAppointment);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId],
+    );
+
+    return parseStringify<Models.Message>(message);
   } catch (error) {
     console.error(error);
     throw error;
